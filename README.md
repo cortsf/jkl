cd2719019818cd9b0cb852368414892f46e1a96a
Franco Cortesi <cortesifranco3@gmail.com>
2024-08-20 05:25:55 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Usage
+
+``` bash
+jkl [extra-args passed to nixos-rebuild]
+```
+
+# Deps
+
+Only `s - scan` requires [git-secrets](https://github.com/awslabs/git-secrets)
+
+
+# To-do
+- Allow users to specify dotfiles path
+- Allow users to specify machine name with something like `machine="$1" && shift`
---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Rebuild shortcuts for nixos";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.${system}.default = pkgs.writeShellApplication {
+        name = "jkl";
+        text = ${builtins.readFile ./jkl}
+      };
+    };
+}
