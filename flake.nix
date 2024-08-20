635b4f7b20f0e4d81db9188213b3a27e2353ce3d
Franco Cortesi <cortesifranco3@gmail.com>
2024-08-20 12:16:22 -0300

    Uncomment flake, remove shebang, add sleep after commit

--- flake.nix
+++ flake.nix
@@ -1,17 +1,17 @@
-# {
-#   description = "Rebuild shortcuts for nixos";
-#   inputs = {
-#     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-#   };
-#   outputs = { self, nixpkgs }:
-#     let
-#       system = "x86_64-linux";
-#       pkgs = nixpkgs.legacyPackages.${system};
-#     in
-#     {
-#       packages.${system}.default = pkgs.writeShellApplication {
-#         name = "jkl";
-#         text = builtins.readFile ./jkl;
-#       };
-#     };
-# }
+{
+  description = "Rebuild shortcuts for nixos";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.${system}.default = pkgs.writeShellApplication {
+        name = "jkl";
+        text = builtins.readFile ./jkl;
+      };
+    };
+}
